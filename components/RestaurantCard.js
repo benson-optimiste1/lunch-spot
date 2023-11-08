04db17da4860c9ba2063d8e24cb7338b2c176e3d
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-08 16:30:31 -0500

    router imported from expo-router

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,27 +1,37 @@
+import { useContext } from "react"
 import { Box, Image, Heading, Text, Pressable } from "@gluestack-ui/themed"
+import { router } from "expo-router"
+import { RestaurantContext } from "../app/_layout"
 
-import { Link } from "expo-router"
 
 export default function RestaurantCard({ restaurant }) {
-    return (
-        <Link href="/details" asChild>
-            <Pressable>
-                <Box
-                    padding={12}
-                    mb={16}
-                    rounded="$xl"
-                    bgColor="$backgroundLight50">
-                    <Image
-                        w="$full"
-                        h={160}
-                        rounded="$lg"
 
-                        alt={restaurant.name}
-                        source={{ uri: restaurant.photo_url }} />
-                    <Heading>{restaurant.name}</Heading>
-                    <Text>{restaurant.address}</Text>
-                </Box>
-            </Pressable>
-        </Link>
+    const { setThisRest } = useContext(RestaurantContext)
+
+    const showDetails = () => {
+        setThisRest(restaurant) // putting restaurant data into context
+        router.push("/details") // navigating to details  page
+    }
+
+    return (
+
+        <Pressable onPress={showDetails}>
+            <Box
+                padding={12}
+                mb={16}
+                rounded="$xl"
+                bgColor="$backgroundLight50">
+                <Image
+                    w="$full"
+                    h={160}
+                    rounded="$lg"
+
+                    alt={restaurant.name}
+                    source={{ uri: restaurant.photo_url }} />
+                <Heading>{restaurant.name}</Heading>
+                <Text>{restaurant.address}</Text>
+            </Box>
+        </Pressable>
+
     )
 }
